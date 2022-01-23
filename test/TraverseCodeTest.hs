7291928e88441788f72647918e11a4a6f48559c2
David Feuer <David.Feuer@gmail.com>
2022-01-22 23:09:52 -0500

    Add lots more instances
    Lots of stuff from `base`, plus polymorphic arrays from
    `array` and `primitive`.

--- test/TraverseCodeTest.hs
+++ test/TraverseCodeTest.hs
@@ -1,10 +1,32 @@
+-- {-# options -ddump-ds #-}
 {-# language TemplateHaskell #-}
-module Main (main) where
+module Main (module Main) where
 
 import Language.Haskell.TH.TraverseCode
 import qualified Data.Functor.Product as FP
 import Data.Functor.Sum
 import qualified Data.Sequence as S
+import Generics.Linear
+import Data.Primitive.SmallArray
+import GHC.Exts (fromList)
+import Test.Tasty.HUnit
+import Test.Tasty
+
+arry0 :: SmallArray a
+arry0 = $$(sequenceCode (fromList []))
+
+arry1 :: a -> SmallArray a
+arry1 a = $$(sequenceCode (fromList [ [||a||]]))
+
+arry2 :: a -> a -> SmallArray a
+arry2 a b = $$(sequenceCode (fromList [[||a||], [||b||]]))
+
+hah :: Maybe a -> MP1 m Maybe a
+hah Nothing = $$(sequenceCode (MP1 Nothing))
+hah (Just x) = $$(sequenceCode (MP1 (Just [||x||])))
+
+ho :: a -> a -> MP1 m [] a
+ho x y = $$(sequenceCode (MP1 [ [||x||], [||y||] ]))
 
 boom :: a -> (Int, a)
 boom a = $$(sequenceCode (12, [|| a ||]))
@@ -40,4 +62,9 @@ seq11 a b c d e  f g h i j k = $$(traverseCode id $ S.reverse (S.fromList
    [ [||a||], [||b||], [||c||], [||d||], [||e||], [||f||], [||g||], [||h||],[||i||],[||j||],[||k||] ]))
 
 main :: IO ()
-main = putStrLn "Test suite not yet implemented."
+main = defaultMain $ testGroup "properties" [
+  testCase "ary" $ do
+    (emptySmallArray :: SmallArray Int) @=? arry0
+    fromList [10 :: Int] @=? arry1 10
+    fromList [20, 30] @=? arry2 20 30
+  ]
